3b2472f382b4859d262468be22faf49efd3dd8e7
alhenriqx <alhenriq@hotmail.com>
2022-06-13 13:21:58 -0700

    fixing files

---
+++ CameraWebServer/MicroSD.cpp
@@ -0,0 +1,109 @@
+#include "FS.h"
+#include "SD_MMC.h"
+
+void removeAllFiles(fs::FS &fs, const char *dirname)
+{
+   File root = fs.open(dirname);
+    if(!root){
+        Serial.println("Failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println("Not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());            
+        } else {            
+            Serial.print("Deleting FILE: ");
+            Serial.print(file.name());
+            Serial.print("  SIZE: ");
+            Serial.println(file.size());            
+            fs.remove(file.name());
+        }
+        file = root.openNextFile();
+    }
+}
+
+
+void createDir(fs::FS &fs, const char * path){
+    Serial.printf("Creating Dir: %s\n", path);
+    if(fs.mkdir(path)){
+        Serial.println("Dir created");
+    } else {
+        Serial.println("mkdir failed");
+    }
+}
+
+void removeDir(fs::FS &fs, const char * path){
+    Serial.printf("Removing Dir: %s\n", path);
+    if(fs.rmdir(path)){
+        Serial.println("Dir removed");
+    } else {
+        Serial.println("rmdir failed");
+    }
+}
+
+void readFile(fs::FS &fs, const char * path, uint8_t **ppbuf, int *pLen){
+    Serial.printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if(!file){
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+
+    size_t size = file.size();
+
+    uint8_t *buf = (uint8_t*)malloc(sizeof(uint8_t) * size);
+
+    if (buf != NULL)
+    {
+      file.read(buf, size);
+    }
+    else
+    {
+      Serial.println("mem alloc to read failed");
+      size = 0;
+    }
+    file.close();
+
+    *ppbuf = buf;
+    *pLen = (int)size;
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("Message appended");
+    } else {
+        Serial.println("Append failed");
+    }
+    file.close();
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # esp32CamProject
 esp32cam project making full use of board
+I have no interest at this time to use the facial recognition - so I repurpose one of the sliders to turn on off the LED.
+Also I noticed these modules run extremely hot - especially when streaming - so I use the internal ADC for temp measurement (highly inaccurate during idle)
+But for my purposes ok once it reports a core temperature above 82 degress C.
+
+I found a great tool to decode the HTML script - and I've added three buttons in hopes of recording some timelapse images and playing back from the es32 module.
